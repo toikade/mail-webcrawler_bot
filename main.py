534f06113d9542750cecb76609f389e782005a05
toikade <toikadeaaron@gmail.com>
2022-11-12 22:18:39 +0100

    fix: fixed the bug showing the state address instead of the city address

--- main.py
+++ main.py
@@ -14,12 +14,15 @@ state_links = [link["href"] for link in table_rows]
 city_links=[]
 for link in state_links:
     city_page = requests.get(base_url+link)
-    city_soup = BeautifulSoup(page.content, 'html.parser')
-    city_table = soup.find_all(id="example")
-    city_rows = table[0].select("tbody tr a")
-    print(page)
+    city_soup = BeautifulSoup(city_page.content, 'html.parser')
+    city_table = city_soup.find_all(id="example")
+    city_rows = city_table[0].select("tbody tr a")
+    print(base_url+link)
+    print('+'*100)
     city_link = [link["href"] for link in city_rows]
     print(city_link)
+    print(len(city_link))
+    print('='*100)
     city_links.append(city_link)
 
 print(city_links)
